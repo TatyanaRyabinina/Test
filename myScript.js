12ef7bd7b3d67bc1c75a5914c849037a00c296d8
TatyanaRyabinina <lugovskaya.tatya@mail.ru>
2017-09-08 12:01:40 +0300

    validation

--- myScript.js
+++ myScript.js
@@ -17,12 +17,26 @@ $(document).ready(function() {
 		
 })
 
+function formSubmit(form) {
+    var isValid = validate(form);
+    if (isValid) {
+		
+	}
+	event.preventDefault();
+}
+
+
 function validate(form) {
-	  inputs = $(form).find(":input"),
-	  $(form).find(".error").remove();
-	  inputs.each(function(i, e) {
-			inputValid = self.validateInput(e);
-	  })
+	var inputs = $(form).find(":input"),
+		valid = true,
+		$(form).find(".error").remove();
+	inputs.each(function(i, e) {
+		inputValid = self.validateInput(e);
+		if (!inputValid) {
+			valid = false;
+		}
+	 })
+	 return valid;
 }
       			
 function validateInput(formElements) {
@@ -30,20 +44,35 @@ function validateInput(formElements) {
 			val = input.val(),
 			maxLength = input.attr("data-max-length"),
 			required = input.attr("required"),
+			pattern = input.attr("pattern"),
 			valid = true,
 			validateErrorMessage = "";
 			
 		if (required && (!val || val.length === 0)) {
 				valid = false;
 				validateErrorMessage = "Empty field!";
-		}
+		} else if ((required || val) && pattern) {
+			if (pattern) {
+				regExp = new RegExp(pattern);
+			}
 			
+			if (regExp && regExp.test) {
+				valid = regExp.test(val);
+				if(!valid) {
+					validateErrorMessage = "Only digits!";
+
+				}
+			}
+		}
 		if (valid && maxLength) {
 			valid = val.length <= maxLength;
 			validateErrorMessage = "Value shouldn't be longer than " + maxLength + " chars";
 		}
 		
 		if (!valid) {
-			input.after("<span class='message error'>" + validateErrorMessage + "</span>");
+			input.after("<span class='error'>" + validateErrorMessage + "</span>");
 		}
+		
+		return valid;
+		
 }
