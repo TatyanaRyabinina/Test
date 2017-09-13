f64e5e9c2ab976a11819ea46da108147bd1c599f
TatyanaRyabinina <lugovskaya.tatya@mail.ru>
2017-09-13 11:08:47 +0300

    final

--- myScript.js
+++ myScript.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
 	$.getJSON("GetCountries.json"
 	).done(function(response)  {
-		console.log(response);
 		$(function(){
 			var availableCountry =[];
 
@@ -38,8 +37,8 @@ function submitInfo(form) {
 		isValid = validate(form);
     if (isValid) {
 		var inputs = form.serializeObject();
-			self.objInfo = JSON.stringify(inputs);		
-		form.html("<label>" + inputs.firstName + " " + inputs.lastName + "</label><br><label>" + inputs.county +"</label><br><label>" + inputs.phone + "</label>");
+		self.objInfo = JSON.stringify(inputs);		
+		form.html("<br><label>" + inputs.firstName + " " + inputs.lastName + "</label><br><label>" + inputs.county +"</label><br><label>" + inputs.phone + "</label><br>");
 		$("#pickupForm>div").attr("hidden", false);
 	}
 	event.preventDefault();
@@ -51,35 +50,64 @@ function submitPickup(form) {
 		firstRadio = $("#one"),
 		isValid;
 	if(firstRadio.prop("checked")) {
-		var objPickup = {};
+		self.objPickup = {};
 		form.html("<label>" + firstRadio.parent().text() + "</label>"); 
 	} else {
 		isValid = validate(form);
 		if (isValid) {
 			var inputs = form.serializeObject();
-				self.objPickup = JSON.stringify(inputs);		
-			form.html("<label>Assistant Pickup</label><br><label>" + inputs.firstName + " " + inputs.lastName + "</label><br><label>" + inputs.email +"</label><br><label>" + inputs.phone + "</label>");
+			self.objPickup = JSON.stringify(inputs);		
+			form.html("<br><label>Assistant Pickup</label><br><label>" + inputs.firstName + " " + inputs.lastName + "</label><br><label>" + inputs.email +"</label><br><label>" + inputs.phone + "</label><br>");
 		}
 	}
 	$("#paymentForm>div").attr("hidden", false);
 
 	event.preventDefault();
-
 }
 
 
 function submitPayment(form){
-	var form = $(form),
+	var self = this,
+		form = $(form),
 		isValid = validate(form);
 	if (isValid) {
 		var inputs = form.serializeObject();
-			self.objPayment = JSON.stringify(inputs);		
-
+		self.objPayment = JSON.stringify(inputs);	
+		$("#submitOrder").removeAttr("disabled");
+		form.html("<h4>Payment Information</h4><label>Card type: VISA</label><br><label>Card ending in " + ((inputs.card).toString()).substr(-4) + "</label><br><label>Expires " + inputs.month + "/" + inputs.year + "</label><br>");
 	}
-		
-	
-			event.preventDefault();
+	event.preventDefault();
 }
+
+
+function submitOrder(){
+	$.when(
+		ajax("SavePersonalInfo.json", objInfo),
+		ajax("SavePickup.json", objPickup),
+		ajax("SavePayment.json", objPayment)
+	).done(function(responseInfo, responsePickup, responsePayment){
+		if(responseInfo && responseInfo[0].status === "success") {
+			localStorage.setItem('orderId', responseInfo[0].data.orderId);
+		}
+		if(responsePickup && responsePickup[0].status === "success") {
+			localStorage.setItem('message', responsePickup[0].data.message);
+		}
+		if(responsePayment && responsePayment[0].status === "success"){
+			location.href = location.origin + responsePayment[0].data.redirectUrl;
+		}
+	});
+}
+
+function ajax(url, data) {
+	return $.ajax({
+				url: url,
+				type: "POST",
+				data: data,
+				contentType: "application/json; charset=utf-8",
+				dataType: "json"
+			})
+}
+
 
 function checkRadio(){
 	if($("#one").prop("checked")) {
@@ -105,66 +133,63 @@ function validate(form) {
 	});
 	
 	selects.each(function(i, e) {
-			dropDownValid = self.validateDropDown(e);
-			if (!dropDownValid) {
-				valid = false;
-			}
-		});
+		dropDownValid = self.validateDropDown(e);
+		if (!dropDownValid) {
+			valid = false;
+		}
+	});
 	
 	 return valid;
 }
       			
-function validateInput(formElements) {
-		var input = $(formElements),
-			val = input.val(),
-			maxLength = input.attr("data-max-length"),
-			required = input.attr("required"),
-			pattern = input.attr("pattern"),
-			valid = true,
-			validateErrorMessage = "";
+function validateInput(formElement) {
+	var input = $(formElement),
+		val = input.val(),
+		maxLength = input.attr("data-max-length"),
+		required = input.attr("required"),
+		pattern = input.attr("pattern"),
+		valid = true,
+		validateErrorMessage = "";
 			
-		if (required && (!val || val.length === 0)) {
-				valid = false;
-				validateErrorMessage = "Empty field!";
-		} else if ((required || val) && pattern) {
-			if (pattern) {
-				regExp = new RegExp(pattern);
-			}
-			
-			if (regExp && regExp.test) {
-				valid = regExp.test(val);
-				if(!valid) {
-					validateErrorMessage = "Not valid!";
-
-				}
+	if (required && (!val || val.length === 0)) {
+			valid = false;
+			validateErrorMessage = "Empty field!";
+	} else if ((required || val) && pattern) {
+		if (pattern) {
+			regExp = new RegExp(pattern);
+		}
+		if (regExp && regExp.test) {
+			valid = regExp.test(val);
+			if(!valid) {
+				validateErrorMessage = "Not valid!";
 			}
 		}
-		if (valid && maxLength) {
-			valid = val.length <= maxLength;
-			validateErrorMessage = "Value shouldn't be longer than " + maxLength + " chars";
-		}
-		
-		if (!valid) {
-			input.after("<span class='error'>" + validateErrorMessage + "</span>");
-		}
-		
-		return valid;
-		
+	}
+	if (valid && maxLength) {
+		valid = val.length <= maxLength;
+		validateErrorMessage = "Value shouldn't be longer than " + maxLength + " chars";
+	}
+	
+	if (!valid) {
+		input.after("<span class='error'>" + validateErrorMessage + "</span>");
+	}
+	
+	return valid;		
 }
 
-function validateDropDown(formElements) {
-		var select = $(formElements),
-			valid = true,
-			selectVal = select.find("option:first-child").text(),
-			validateErrorMessage = "Not valid";
+function validateDropDown(formElement) {
+	var select = $(formElement),
+		valid = true,
+		selectVal = select.find("option:first-child").text(),
+		validateErrorMessage = "Not valid";
 			
-		if (!selectVal) {
-			valid = false;
-		}
-
-		if (!valid) {
-			select.after("<span class='error'>" + validateErrorMessage + "</span>");
-		}
-
-		return valid;
+	if (!selectVal) {
+		valid = false;
 	}
+
+	if (!valid) {
+		select.after("<span class='error'>" + validateErrorMessage + "</span>");
+	}
+
+	return valid;
+}
