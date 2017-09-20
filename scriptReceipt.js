e0c3122e62fccd05f430fe98cb3fe1c7a2fcd22f
TatyanaRyabinina <lugovskaya.tatya@mail.ru>
2017-09-20 15:29:43 +0300

    Delete scriptReceipt.js

--- scriptReceipt.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-	var message = localStorage.getItem('message'),
-		orderId = localStorage.getItem('orderId');
-	if(message && orderId) {
-		$('body').html("<label>" + message + "</label><br><label>Order Id " + orderId + "</label>");
-	}
-})
